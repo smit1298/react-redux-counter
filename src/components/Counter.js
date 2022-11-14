e1e40ae7f54550cdc15f75c013de2646080e7dbd
Temitope Samson Isaiah <61621188+smit1298@users.noreply.github.com>
2022-11-14 10:21:28 +0100

    react ounter project completed using redux for state management

---
+++ src/components/Counter.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement, reset } from "../redux/counter"
+import './Counter.css';
+
+const Counter = () => {
+	const { count } = useSelector((state) => state.counter);
+    console.log(count)
+	const dispatch = useDispatch()
+
+   
+	return (
+		<div className="container">
+			<div className="counter">
+				<div className="digits">{count}</div>
+				<div className="console">
+					<button
+						className="btn"
+						onClick={()=> dispatch(decrement())}>
+						Reduce Number
+					</button>
+					This is {count}
+					<button
+						className="btn"
+						onClick={()=> dispatch(increment())}>
+						Increase number
+					</button>
+				</div>
+				<button
+					className="reset btn"
+					onClick={()=> dispatch(reset())}>
+					Reset
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Counter;
